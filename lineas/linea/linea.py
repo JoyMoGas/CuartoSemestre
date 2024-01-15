d2af4b9d2eee71e7d0fc3f573eb2e3e09d084ef3
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2024-01-15 14:50:33 -0700

    Ultimo Cambio 15/01

---
+++ lineas/linea/linea.py
@@ -0,0 +1,15 @@
+def linea(x,m,b):
+    resultado = (m*x) + b
+    return resultado
+
+def calcula_y(X,m,b):
+    Y = [linea(x, m, b) for x in X]
+    return Y
+
+if __name__ == "__main__":
+    X = [x for x in range(10)]
+    m = 2
+    b = 3
+    Y = calcula_y(X,m,b)
+    print(X,Y)
+

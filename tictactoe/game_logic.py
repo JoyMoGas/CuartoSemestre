9816a1eb1bd76a23d4e1b62eede72053bfea8cb2
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2024-01-22 14:49:17 -0700

    Cambio con errores 22/01

--- tictactoe/game_logic.py
+++ tictactoe/game_logic.py
@@ -18,22 +18,55 @@ def display_tablero(tablero: dict):
 def ia(board:dict):
     ocuppied = True
     while ocuppied == True:
-        r = random.choices(board.keys())
+        r = random.choices(list(board.keys()))
         if board[r] == str(r):
             board[r] = "O"
             ocuppied = False
 
+def check_winner(tab, lista_lineas):
+    for cmb in lista_lineas:
+        if tab[cmb[0]] == tab[cmb[1]] == tab[cmb[2]]:
+            return True
+    return False
+
 def game(tab:dict):
-    while True:
+    lista_combinaciones = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,4],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    turnos = 0
+    while turnos < 10:
         display_tablero(tab)
-        usuario = input("Escoja celda: ")
-        usuario = int(usuario)
-        if usuario in tab:
-            if tab[usuario] == str(usuario):   
-                tab[usuario] = "X"
+        correcto = juega_usuario(tab)
+        if correcto:
+            gana = check_winner(tab, lista_combinaciones)
+            if gana == True:
+                print("Ganaste!")
+                break
             else:
-                print(f"Posición {usuario} ocupada")
-                print("Elija otra opción")
+                print("Ganó la IA")
+            ia(tab)
+        turnos += 1
+
+
+def juega_usuario(tab):
+    turno_correcto = False
+    usuario = input("Escoja celda: ")
+    usuario = int(usuario)
+    if usuario in tab:
+        if tab[usuario] == str(usuario):   
+            tab[usuario] = "X"
+            turno_correcto = True
+        else:
+            print(f"Posición {usuario} ocupada")
+            print("Elija otra opción")
+    return turno_correcto
 
 # print(f"tablero :{tablero}")
 # print(f"tab_dict :{tab_dict}")

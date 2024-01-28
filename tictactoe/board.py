dd861b56df421409e7388b9d03b76181f968b240
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2024-01-27 20:48:24 -0700

    Se cambio el nombre de class_logic_class

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -1,6 +1,8 @@
 from colorama import Fore, Back, Style, Cursor
+tablero = [x for x in range(0,9)] #0,1,2,3...8
+tab_dict= {x:str(x) for x in tablero}
 
-def display_tablero(tab: dict):
+def display_tablero(tablero:dict):
     reset = Style.RESET_ALL
     bg = Back.WHITE
     blue = Fore.BLUE
@@ -9,20 +11,25 @@ def display_tablero(tab: dict):
     o_color = Fore.GREEN
     X = x_color + "X"
     O = o_color + "O"
-    BD = board_color + "---------"
+    BD = board_color + "-------"
     BS = board_color + "|"
     d = {}
-    for k,v in tab.items():
+    for k,v in tablero.items():
         if v == "X":
             d[k] = X + BS
         elif v == "O":
             d[k] = O + BS
+        else:
+            d[k] = blue + str(k) + BS
     Cursor.POS(10,5)
     print(f"{bg}{BD}{reset}")
-    print(f"{bg}|{d[0]}{d[1]}|{d[2]}|{reset}")
+    print(f"{bg}|{d[0]}{d[1]}{d[2]}{reset}")
     print(f"{bg}{BD}{reset}")
-    print(f"{bg}|{d[3]}|{d[4]}|{d[5]}{reset}")   
-    print(f"{bg}{BD}{reset}")    
-    print(f"{bg}|{d[6]}|{d[7]}{d[8]}|{reset}")
-    print(f"{bg}{BD}{reset}")  
+    print(f"{bg}|{d[3]}{d[4]}{d[5]}{reset}")
+    print(f"{bg}{BD}{reset}")
+    print(f"{bg}|{d[6]}{d[7]}{d[8]}{reset}")
+    print(f"{bg}{BD}{reset}")
     print(Style.RESET_ALL)
+
+if __name__ == "__main__":
+    display_tablero(tab_dict)
--- tictactoe/game_logic.py
+++ tictactoe/game_logic.py
@@ -2,75 +2,105 @@
 Lógica del programa del gato
 '''
 import random
+import board
 
-tablero = [x for x in range(0, 9)]  # 0,1,2,3...8
-tab_dict = {x: str(x) for x in tablero}
+tablero = [x for x in range(0,9)] #0,1,2,3...8
+tab_dict= {x:str(x) for x in tablero}
 
-def display_tablero(tab: dict):
+def display_tablero(tab:dict):
     print(f" {tab[0]} | {tab[1]} | {tab[2]} ")
     print("-----------")
     print(f" {tab[3]} | {tab[4]} | {tab[5]} ")
     print("-----------")
     print(f" {tab[6]} | {tab[7]} | {tab[8]} ")
 
-def ia(board: dict):
-    occupied = True
-    while occupied:
+def ia(board:dict):
+    ocuppied = True
+    while ocuppied == True:
         r = random.choice(list(board.keys()))
-        if board[r] == str(r):  # Si está libre
+        if board[r] == str(r): # Si está libre
             board[r] = "O"
-            occupied = False
-
+            ocuppied = False
 def juega_usuario(tab):
     turno_correcto = False
     usuario = input("Escoja celda:")
     usuario = int(usuario)
     if usuario in tab:
         if tab[usuario] == str(usuario):
-            tab[usuario] = "X"
+            tab[usuario]="X"
             turno_correcto = True
         else:
             print(f"Posición {usuario} ocupada")
             print("Eliga otra opción")
-    return turno_correcto
+    return turno_correcto    
 
-def check_winner(tab, lista_lineas):
+def check_winner(tab,lista_lineas):
     for cmb in lista_lineas:
-        if tab[cmb[0]] == tab[cmb[1]] == tab[cmb[2]]:
+        if tab[cmb[0]]==tab[cmb[1]]==tab[cmb[2]]:
             return True
     return False
 
-def game(tab: dict):
+def game(tab:dict):
+    diccionario = {'ganador':''}
     lista_combinaciones = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
     ]
     turnos = 0
-    while turnos < 9:
-        display_tablero(tab)
+    while turnos < 8:
+        board.display_tablero(tab)
         correcto = juega_usuario(tab)
         if correcto:
-            turnos += 1
-            gana = check_winner(tab, lista_combinaciones)
-            if gana:
+            turnos +=1
+            gana = check_winner(tab,lista_combinaciones)
+            if gana == True:
+                diccionario['ganador'] = "jugador"
                 print("¡Ganaste!")
                 break
-            if turnos == 9:
-                print("¡Es un empate!")
-                break
             ia(tab)
-            gana = check_winner(tab, lista_combinaciones)
-            if gana:
-                display_tablero(tab)
-                print("La IA ganó. Mejor suerte la próxima vez.")
+            gana = check_winner(tab,lista_combinaciones)
+            if gana == True:
+                diccionario['ganador'] = 'IA'
+                print("¡Ganó la IA!")
                 break
             turnos += 1
+    display_tablero(tab)
+    return diccionario
+
+def display_score(s:dict, d:dict):
+    if d['ganador'] != '':
+        print(f"Ganó:{d['ganador']}")
+        s[ d['ganador'] ] += 1
+    else:
+        s['Empates'] += 1
+        print("¡Empate!")
+    print(f"<<Jugador:{s['jugador']}>> <<IA:{s['IA']}>> <<Empates:{s['Empates']}>>")
+
+def jugar_otra_vez():
+    jugar = True
+    otra_vez = input("¿Quieres jugar otra vez? (S/N): ")
+    otra_vez = otra_vez.upper()
+    if (otra_vez != 'S'):
+        jugar = False
+        print("¡Gracias por jugar!")
+    return jugar
+
+def game_cycle():
+    score = {'jugador': 0, 'IA': 0, 'Empates':0}
+    continuar = True
+    while continuar:
+        tab_dict= {x:str(x) for x in tablero}
+        d = game(tab_dict)
+        display_score(score, d)
+        
+        continuar = jugar_otra_vez()
+        
 
 if __name__ == "__main__":
-    game(tab_dict)
+    game_cycle()

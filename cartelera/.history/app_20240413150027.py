8e34a42b44928b5147ef09c2ecb8dbda7b203ec3
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2024-04-13 16:45:10 -0700

    Pagina Años terminada, falta pagina para cada genero

---
+++ cartelera/.history/app_20240413150027.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+from funciones import carga_csv, peliculas_mas_recientes
+from funciones import crea_diccionario_peliculas
+from funciones import crea_diccionario_genero
+from funciones import crea_diccionario_anios
+from datetime import datetime
+import os
+
+
+archivo_cartelera = 'cartelera_2024.csv'
+app = Flask(__name__)
+cartelera = carga_csv(archivo_cartelera)
+diccionario_peliculas=crea_diccionario_peliculas(cartelera)
+diccionario_generos = crea_diccionario_genero(cartelera)
+diccionario_anios = crea_diccionario_anios(cartelera)
+
+
+@app.route("/")
+def index():
+    global cartelera
+    lista_peliculas = peliculas_mas_recientes(cartelera)
+    return render_template("index.html",lista=lista_peliculas)
+
+@app.route("/generos")
+def generos():
+    return render_template("generos.html",dicc_generos=diccionario_generos)
+
+@app.route("/genero")
+def genero():
+    return render_template("genero.html")
+
+@app.route("/anios")
+def anio():
+    return render_template("anio.html", dicc_anios=diccionario_anios)
+
+@app.route("/alfabetico")
+def alfabetico():
+    return render_template("alfabetico.html")
+
+@app.route("/pelicula/<id>")
+def pelicula(id:str):
+    if id in diccionario_peliculas:
+        pelicula = diccionario_peliculas[id]  
+        print(f"movie={pelicula['titulo']}")  
+        return render_template("movie.html",movie=pelicula)
+    else:
+        return render_template("no_existe.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
